18985d2648eccb9e282735bd7342b50fca60de1f
Padwick25 <padwick25@gmail.com>
2018-02-08 17:24:48 +0000

    pls work mr script

--- Deploy.sh
+++ Deploy.sh
@@ -13,16 +13,16 @@ docker rmi -f $(docker images -q)
 echo making new build directory
 mkdir Build
 echo moving files into build
-mv ~/target ~/Build
-mv ~/frontend ~/Build
-#movev deploy.sh?
-cd ~/Build/target
+mv /home/ubuntu/target /home/ubuntu/Build
+mv /home/ubuntu/frontend /home/ubuntu/Build
+mv
+cd /home/ubuntu/Build/target
 echo Building backend image
 docker build -t backend .
 echo Running backend image
 docker run -d -p 3000:8080 backend
 echo Backend now available at NODE-IP:3000
-cd ~/Build/frontend
+cd /home/ubuntu/Build/frontend
 echo Building frontend image
 docker build -t frontend .
 echo Running frontend image
